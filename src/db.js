cf0ac082b7b56a2f19c67cbcb6f4ab40c6f40bae
JuanTosso <juantosso89@gmail.com>
2023-06-20 10:02:33 -0300

    last commit

--- src/db.js
+++ src/db.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-  {
-    logging: false, //es para dejar de imprimir en consola lenguaje SQL
-    native: false,
-  }
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, //es para dejar de imprimir en consola lenguaje SQL
+  native: false,
+});
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+//   {
+//     logging: false, //es para dejar de imprimir en consola lenguaje SQL
+//     native: false,
+//   }
+// );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
